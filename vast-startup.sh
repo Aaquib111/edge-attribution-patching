3685f4b05723dc1421633cc0967792f48c4544bc
Aaquib111 <aaquibahm@gmail.com>
2023-09-02 19:39:44 +0000

    Working acdcpp

---
+++ vast-startup.sh
@@ -0,0 +1,6 @@
+apt-get update && apt-get upgrade
+git clone https://github.com/Aaquib111/acdcpp.git
+cd acdcpp
+apt-get install graphviz-dev
+pip install git+https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git
+pip install -r requirements.txt
