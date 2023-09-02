e33ca6a8dbc31e1489b65b58473a778d1d07a7e1
Aaquib111 <aaquibahm@gmail.com>
2023-09-02 19:43:10 +0000

    Init

--- vast-startup.sh
+++ vast-startup.sh
@@ -1,4 +1,8 @@
 apt-get update && apt-get upgrade
+git config --global user.name [INSERT NAME]
+git config --global user.email [INSERT EMAIL]
+git config --global github.user [INSERT USERNAME]
+git config --global github.token [INSERT TOKEN]
 git clone https://github.com/Aaquib111/acdcpp.git
 cd acdcpp
 apt-get install graphviz-dev
