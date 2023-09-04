d50965148092498cf0747995fa3176dbbe70ba9f
Aaquib Syed <47124521+Aaquib111@users.noreply.github.com>
2023-09-03 22:08:31 -0400

    Update vast-startup.sh

--- vast-startup.sh
+++ vast-startup.sh
@@ -1,10 +1,10 @@
 apt update && apt upgrade
 git init
-git remote set-url origin https://Aaquib111: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@github.com/Aaquib111/acdcpp.git
+git remote set-url origin https://Aaquib111:[TOKEN]@github.com/Aaquib111/acdcpp.git
 git config --global user.name Aaquib111
-git config --global user.email aaquibahm@gmail.com 
+git config --global user.email [EMAIL]
 git config --global github.user Aaquib111 
-git config --global github.token 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
+git config --global github.token [TOKEN]
 apt install graphviz
 apt install graphviz-dev
 pip install git+https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git
