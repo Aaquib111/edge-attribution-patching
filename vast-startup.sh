098e45797753a4a7859c07454c7d17f2b1319a7a
Aaquib111 <aaquibahm@gmail.com>
2023-09-05 21:08:21 +0000

    IOI Experiments Fixed

--- vast-startup.sh
+++ vast-startup.sh
@@ -2,10 +2,10 @@ apt update && apt upgrade
 git init
 git remote set-url origin https://Aaquib111:[TOKEN]@github.com/Aaquib111/acdcpp.git
 git config --global user.name Aaquib111
-git config --global user.email [EMAIL]
+git config --global user.email email 
 git config --global github.user Aaquib111 
-git config --global github.token [TOKEN]
+git config --global github.token [TOKEN] 
 apt install graphviz
 apt install graphviz-dev
-pip install git+https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git
+git clone https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git
 pip install -r requirements.txt
