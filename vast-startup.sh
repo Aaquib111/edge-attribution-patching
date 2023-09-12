a353745dd249c8217209224e082919cf66f94417
Aaquib111 <aaquibahm@gmail.com>
2023-09-12 11:46:07 -0400

    Added utils files

--- vast-startup.sh
+++ vast-startup.sh
@@ -1,11 +1,24 @@
+github_username=# INSERT USERNAME HERE
+github_token=#INSERT TOKEN HERE
+email=# INSERT EMAIL HERE
+
 apt update && apt upgrade
+
 git init
-git remote set-url origin https://Aaquib111:[TOK]@github.com/Aaquib111/acdcpp.git
-git config --global user.name Aaquib111
-git config --global user.email email 
-git config --global github.user Aaquib111 
-git config --global github.token [TOK]
+git remote set-url origin https://Aaquib111:$github_token@github.com//acdcpp.git
+git config --global user.name $github_username
+git config --global user.email $email
+git config --global github.user $github_username 
+git config --global github.token $github_token
+
 apt install graphviz
 apt install graphviz-dev
-git clone https://github.com/ArthurConmy/Automatic-Circuit-Discovery.git
+
+cd ..
+git clone https://github.com/Aaquib111/Automatic-Circuit-Discovery.git
+cd acdcpp/Automatic-Circuit-Discovery/
+git submodule init
+git submodule update
+git pull origin master
+
 pip install -r requirements.txt
